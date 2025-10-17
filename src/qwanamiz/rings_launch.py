c2175413ca718b5c5f32cec752eea4fd726a235f
SamBcht <148474661+SamBcht@users.noreply.github.com>
2025-10-17 10:41:44 -0400

    all files upadate

--- src/qwanamiz/rings_launch.py
+++ src/qwanamiz/rings_launch.py
@@ -29,7 +29,7 @@ import datetime
 import napari
 from collections import defaultdict
 
-sampleID = "L20_F07-1M2-Sc6"
+sampleID = "L20_F07-1M2-Sc7"
 output_folder = "C:/Users/sambo/Desktop/QWAnamiz_store/qwanamiz_dev"
 
 
